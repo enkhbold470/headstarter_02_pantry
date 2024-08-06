93e559214c5eea822bd01456b9170bf5ab1daa03
Enkhbold470 <enkhbold470@gmail.com>
2024-08-06 15:37:02 -0700

    initial

---
+++ firebase.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: `${process.env.apiKey}`,
+//   authDomain: `${process.env.authDomain}`,
+//   projectId: `${process.env.projectId}`,
+//   storageBucket: `${process.env.storageBucket}`,
+//   messagingSenderId: `${process.env.messagingSenderId}`,
+//   appId: `${process.env.appId}`,
+//   measurementId: `${process.env.measurementId}`,
+// };
+const firebaseConfig = {
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  projectId: `pantry-tracker-a8706`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export { firestore };
