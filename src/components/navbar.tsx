fcde2cd93e61e38e621181a285b2906b73739648
Enkhbold Ganbold <53986637+enkhbold470@users.noreply.github.com>
2024-08-08 02:32:29 -0700

    feat: Add scan grocery page and camera component
    The code changes include:
    - Added a new page for scanning groceries
    - Added a camera component for taking photos
    - Updated the global CSS file
    
    Recent user commits:
    - some major change
    
    Recent repository commits:
    - some major change
    - Initial commit from Create Next App

---
+++ src/components/navbar.tsx
@@ -0,0 +1,42 @@
+"use client";
+import * as React from "react";
+import Link from "next/link";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+const navbarItems = [
+  { name: "Нүүр", link: "/" },
+  { name: "Хүнс Оруулах", link: "/scanGrocery" },
+  { name: "Шим Тэжээл", link: "/nutrition" },
+  { name: "Хоолны Жор Бэлдэх", link: "/recipes" },
+];
+
+export function NavigationMenuDemo() {
+  return (
+    <div className=" flex justify-center m-2 p-2">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            {navbarItems.map((item, index) => {
+              return (
+                <Link key={index} href={item.link} legacyBehavior passHref>
+                  <NavigationMenuLink
+                    className={`${navigationMenuTriggerStyle()}`}
+                  >
+                    {item.name}
+                  </NavigationMenuLink>
+                </Link>
+              );
+            })}
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+}
