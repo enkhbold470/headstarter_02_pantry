788888ba790084eb4869e499c74307bcefd491de
Enkhbold470 <enkhbold470@gmail.com>
2024-08-09 17:04:36 -0700

    chore: Add scan grocery page, camera component, and dependencies

--- src/app/scanGrocery/form.jsx
+++ src/app/scanGrocery/form.jsx
@@ -1,51 +1,138 @@
-"use client";
+"use client"
+import React, { useState, useEffect } from "react"
+import { firestore } from '@/lib/firebase'
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-
-import { Button } from "@/components/ui/button";
 import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+  collection,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  deleteDoc,
+  getDoc,
+} from 'firebase/firestore'
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
 
-export default function AddItems() {
-  // ...
 
+export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+  const [inventory, setInventory] = useState([])
+  const [itemName, setItemName] = useState('')
+
+  const [formSuccess, setFormSuccess] = useState(false)
+  const [formSuccessMessage, setFormSuccessMessage] = useState("")
+
+  const handleInput = (e) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [fieldName]: fieldValue
+    }));
+  }
+
+  const submitForm = (e) => {
+    // We don't want the page to refresh
+    e.preventDefault()
+
+    const formURL = e.target.action
+    const data = new FormData()
+
+    // Turn our formData state into data we can use with a form submission
+    Object.entries(formData).forEach(([key, value]) => {
+      data.append(key, value);
+    })
+
+    // POST the data to the URL of the form
+    fetch(formURL, {
+      method: "POST",
+      body: data,
+      headers: {
+        'accept': 'application/json',
+      },
+    }).then((response) => response.json())
+      .then((data) => {
+        setFormData({
+          name: "",
+          email: "",
+          message: ""
+        })
+
+        setFormSuccess(true)
+        setFormSuccessMessage(data.submission_text)
+      })
+  }
+
+  // use effect for firebase
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc) => {
+      inventoryList.push({ name: doc.id, ...doc.data() })
+    })
+    setInventory(inventoryList)
+  }
+
+  useEffect(() => {
+    updateInventory()
+  }, [])
+  // add item to inventory
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data()
+      await setDoc(docRef, { quantity: quantity + 1 })
+    } else {
+      await setDoc(docRef, { quantity: 1 })
+    }
+    await updateInventory()
+  }
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data()
+      if (quantity === 1) {
+        await deleteDoc(docRef)
+      } else {
+        await setDoc(docRef, { quantity: quantity - 1 })
+      }
+    }
+    await updateInventory()
+  }
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-  );
+    <div>
+      <h1>Contact form</h1>
+      {formSuccess ?
+        <div>{formSuccessMessage}</div>
+        :
+        <form method="POST" action="/api/" onSubmit={submitForm}>
+          <div>
+            <label>Name</label>
+            <input type="text" name="name" onChange={handleInput} value={formData.name} />
+          </div>
+
+          <div>
+            <label>Email</label>
+            <input type="text" name="email" onChange={handleInput} value={formData.email} />
+          </div>
+
+          <div>
+            <label>Message</label>
+            <textarea name="message" onChange={handleInput} value={formData.message}></textarea>
+          </div>
+
+          <button type="submit">Send message</button>
+        </form>
+      }
+    </div>
+  )
 }
