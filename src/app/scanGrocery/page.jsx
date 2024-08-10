44c004f98c383d29d7c93c7e90ac8fce7844c2e3
Enkhbold470 <53986637+enkhbold470@users.noreply.github.com>
2024-08-09 17:04:36 -0700

    chore: Add scan grocery page, camera component, and dependencies

--- src/app/scanGrocery/page.jsx
+++ src/app/scanGrocery/page.jsx
@@ -1,9 +1,15 @@
+import Contact from './form';
+import { DataTableDemo } from './table';
 export default function ScanGrocery() {
   return (
     <main>
       {/* <Input placeholder="Хүнсний нэр" /> */}
       <h1>Scan Grocery</h1>
+      <Contact />
+      <div className='p-6 border border-white-500'>
 
+        <DataTableDemo />
+      </div>
       {/* <AddItems /> */}
 
       {/* <CameraComponent /> */}
