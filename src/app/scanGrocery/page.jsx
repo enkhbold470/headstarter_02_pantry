6548be823002fe69646431c08011a2dcce5565d7
Enkhbold470 <53986637+enkhbold470@users.noreply.github.com>
2024-08-10 17:10:30 -0700

    chore: Update scan grocery page and camera component

--- src/app/scanGrocery/page.jsx
+++ src/app/scanGrocery/page.jsx
@@ -1,13 +1,24 @@
-import Contact from './form';
+import { Button } from '@/components/ui/button';
 import { DataTableDemo } from './table';
+import Link from 'next/link';
 export default function ScanGrocery() {
   return (
     <main>
-      {/* <Input placeholder="Хүнсний нэр" /> */}
-      <h1>Scan Grocery</h1>
-      <Contact />
-      <div className='p-6'>
 
+      {/* <Contact /> */}
+      <div className='p-6  '>
+        <div className='flex justify-between p-4'>
+          <Button>
+            <Link href="/scanGrocery/add">
+              Add Item Manually
+            </Link>
+          </Button>
+          <Button>
+            <Link href="/scanGrocery/takepic">
+              Barcode Scan
+            </Link>
+          </Button>
+        </div>
         <DataTableDemo />
       </div>
       {/* <AddItems /> */}
