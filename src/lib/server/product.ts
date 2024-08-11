bbc67bd6d8258b00b3a4251d908d9dd866ca16ac
Enkhbold Ganbold <enkhbold470@gmail.com>
2024-08-10 22:15:09 -0700

    fixed issue of POST request consistantly

--- src/lib/server/product.ts
+++ src/lib/server/product.ts
@@ -1,12 +1,17 @@
 "use server";
-import axios from "axios";
 
 export async function getProductData(barcode: string) {
   const url = `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`;
-  const response = await axios.get(url);
+  const response = await fetch(url, { cache: "no-store" });
 
-  if (response.data.status === 1) {
-    return response.data.product;
+  if (!response.ok) {
+    throw new Error("Product not found");
+  }
+
+  const data = await response.json();
+
+  if (data.status === 1) {
+    return data.product;
   } else {
     throw new Error("Product not found");
   }
